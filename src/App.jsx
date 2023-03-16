2197d549c5a1051b327b8ba3e42a650ef5e1a233
EdsonSaulFajardo <fajardo_solano@ucol.mx>
2023-03-16 13:22:05 -0600

    Agregando tailwind configuration y borramos los comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -18,73 +18,35 @@ function App() {
    // console.log(todo)
   }
 
+  function deleteAll(){
+    console.log("funcion de borrado pendiente");
+    console.log();
+  }
+
   return (
-    <div      
-        style={{
-          maxWidth: "600px",
-          marginLeft:"auto",
-          marginRight:"auto",
-          
-      }}
-      >
-      <h1 style={{textAlign:"center"}}>Rect ToDo</h1>
-      <form onSubmit={handleSubmit}>
+    <>
+    <div className="container m-auto">
+      <h1 style={{textAlign:"center"}}  className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">React ToDo</h1>
+      <form onSubmit={handleSubmit} className="my-5">
         <input 
           name="todo" 
           value={todo}
           style={{width:"100%"}} 
           onChange={handleChange}
+          className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Ingresa los valores"
       />
       </form>
-      <ul>
+      <ul className="my-5">
 
         {todos.map((todo, index) => (
           <li key={index}>{todo}</li>
         ))}
       </ul>
+      <button className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600" onClick={deleteAll}>Borrar todo</button>
     </div>
+    
+    </>
   )
 }
 
 export default App
-
-// import { useState } from 'react'
-// function App() {
-//   const [todos, setTodos] = useState([]);
-//   const [todo, setTodo] = useState("");
-// function handleSubmit(event)
-// {
-//   event.preventDefault();
-//   setTodos(todos.concat(todo));
-//   setTodo("");
-
-//   function handleChange(event){
-//     setTodo(event.target.value);
-//   }
-// }
-
-//   return (
-//     <div
-//       style= {{
-//         maxWidth: "600px",
-//         marginLeft: "auto",
-//         marginRight: "auto",
-
-//       }}>
-//       <h1 style={{ textAlign: "center" }}> React ToDo </h1>
-//       <form onSubmit={handleSubmit}>
-        
-//       <input name="todo" 
-//         value= {todo}
-//         style={{width: 100%}}
-//         onChange= {handleChange}
-
-//      />
-//      </form>
-//      <ul>
-//       {todos.map((todo, ndex)==> (<li key={index}>{todo}</li>))}
-//       </ul>
-//     </div>
-//   );
-// }
-// export default App;
